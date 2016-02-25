18c14e9efc78730a1ee8421e0038a14933696720
fernando.sanchez <fsm_031@hotmail.com>
2016-02-25 10:09:54 +0100

    CLASE PROPIA  QUE ACTUA COMO NUESTRO BROADCASTRECIVER. Recibe los mensajes que llegan a nuestra aplicación a través del GCM. Terminada y comentada (los métodos de gestión de mensajes serán implementados en la aatividad 8)

--- app/src/main/java/damp_2/utad/actividad7_fernando_sanchez_martinez/MiWakefullBroadcastReceiver.java
+++ app/src/main/java/damp_2/utad/actividad7_fernando_sanchez_martinez/MiWakefullBroadcastReceiver.java
@@ -6,12 +6,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
-
+/**
+ * CLASE ENCARGADA DE RECIVIR LOS MENSAJES EN NUESTRA APLICACIÓN ANDROID
+ */
 public class MiWakefullBroadcastReceiver extends WakefulBroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         //Log.v("GcmBroadcastReceiver", "HE RECIBIDO!!!!");
         // Explicitly specify that GcmIntentService will handle the intent.
+        /**
+         * Se le pasa el mensaje como intent a la clase gestionadora de mensajes que hereda de inten service
+         */
         ComponentName comp = new ComponentName(context.getPackageName(), MiIntentService.class.getName());
         // Start the service, keeping the device awake while it is launching.
         startWakefulService(context, (intent.setComponent(comp)));
