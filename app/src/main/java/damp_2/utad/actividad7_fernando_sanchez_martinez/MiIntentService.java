dccbaba6089f958f744f7fe3adec713251fc6bc5
fernando.sanchez <fsm_031@hotmail.com>
2016-02-25 09:41:29 +0100

    CLASE MI INTENT SERVICE TERMINADA Y COMENTADA. (queda en blanco el método dde procesamiento de mensajes que se usará en la actividad 8)

--- app/src/main/java/damp_2/utad/actividad7_fernando_sanchez_martinez/MiIntentService.java
+++ app/src/main/java/damp_2/utad/actividad7_fernando_sanchez_martinez/MiIntentService.java
@@ -1,7 +1,6 @@
 package damp_2.utad.actividad7_fernando_sanchez_martinez;
 
 import android.app.IntentService;
-import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,41 +8,52 @@ import android.util.Log;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 
-
+/**
+ * CLASE QUE GESTIONA LA MUESTRA DE LOS MENSAJES CUANDO SON RECIBIDOS POR EL BRODCASTER RECIVER
+ */
 public class MiIntentService extends IntentService {
 
+    /**
+     * Se declaran los parámetros del gcm_intent_service
+     */
     public static final String GCM_INTENT_SERVICE = "GcmIntentService";
     public static final String GCM_SEND_ERROR = "Send error: ";
     public static final String GCM_DELETED_MESSAGE = "Deleted messages on server: ";
     public static final String GCM_RECEIVED = "Received: ";
     public static final String EXTRA_MESSAGE = "message";
 
-    public static final int NOTIFICATION_ID = 1;
+   // public static final int NOTIFICATION_ID = 1;
 
     private static final String TAG = MiIntentService.class.getSimpleName();
 
-    private NotificationManager notificationManager;
+   // private NotificationManager notificationManager;
 
+    /**
+     * constructor de la clase que hereda con un super los atributos del GCM_IntentService
+     */
     public MiIntentService(){
         super(GCM_INTENT_SERVICE);
     }
 
+    /**
+     * Metodo que se ejecuta cuando se lanza el intent, en éste caso nuestra clase gestionadora
+     * @param intent
+     */
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.i(TAG, "new push");
 
         Bundle extras = intent.getExtras();
+        //OBTENEMOS LA INSANCIA DEL GCM
         GoogleCloudMessaging googleCloudMessaging = GoogleCloudMessaging.getInstance(this);
-        // The getMessageType() intent parameter must be the intent you received
-        // in your BroadcastReceiver.
+        /**
+         * El mensaje que se recive (getMessageType) es el intent que se le pasa desde el MiBroadcastreciver
+         */
         String messageType = googleCloudMessaging.getMessageType(intent);
 
-        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
-            /*
-             * Filter messages based on message type. Since it is likely that GCM
-             * will be extended in the future with new message types, just ignore
-             * any message types you're not interested in, or that you don't
-             * recognize.
+        if (!extras.isEmpty()) {  // Siempre que no esté vacio
+            /**
+             * Se hace un filtrado de los tipos de mensaje que pùeden ser recividos
              */
             if (GoogleCloudMessaging.
                     MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
@@ -59,16 +69,15 @@ public class MiIntentService extends IntentService {
                 Log.i(TAG, "Received: " + extras.toString());
             }
         }
-        // Release the wake lock provided by the WakefulBroadcastReceiver.
-        MiWakefullBroadcastReceiver.completeWakefulIntent(intent);
+
+        MiWakefullBroadcastReceiver.completeWakefulIntent(intent);//Se completa la comprobacion del lanzado
+
     }
 
-    // Put the message into a notification and post it.
-    // This is just one simple example of what you might choose to do with
-    // a GCM message.
+
     private void processNotification(String type, Bundle extras) {
 
-
+//AQUI SE PROCESARIAN LOS MENSAJES CUANDO SON PULSADOS( ACTIVIDAD8)
 
     }
 }
